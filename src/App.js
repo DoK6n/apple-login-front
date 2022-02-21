948eea1cf5ee4df723c79b47ecd6772d5b57d5ba
Dok6n <ehrbs2018@gmail.com>
2022-02-21 22:51:11 +0900

    feat: Add apple login & deploy scripts

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const config = {
+    client_id: process.env.REACT_APP_APPLE_CLIENT_ID, // service ID
+    redirect_uri: process.env.REACT_APP_APPLE_REDIRECT_URI, // service ID와 함께 등록한 Website URLs의 Return URLs
+    response_type: 'code id_token',
+    state: 'origin:web', // 선택사항
+    scope: 'name email', // 이름과 이메일을 apple에게
+    response_mode: 'form_post',
+    m: 11,
+    v: '1.5.4'
+  };
+  const queryString = Object.entries(config)
+    .map(([key, value]) => `${key}=${encodeURIComponent(value)}`)
+    .join('&');
+  const appleLoginUrl = `https://appleid.apple.com/auth/authorize?${queryString}`;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <a href={appleLoginUrl}>login</a>
       </header>
     </div>
   );
